3747f3ef095e70c8f17ca659ec4d6dfc47232d7b
lgbya <lgbya1105882406@aliyun.com>
2020-07-24 23:47:01 +0800

    认证后的消息提醒

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,4 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 // Email 认证相关路由
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
-Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
