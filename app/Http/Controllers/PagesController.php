284fe0789ac5cda077cd0f356b70039b2109d8da
lgbya <lgbya1105882406@aliyun.com>
2020-07-24 23:02:47 +0800

    邮箱认证

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PagesController extends Controller
 {
     public function root()
     {
+//        var_dump(Auth::user()->hasVerifiedEmail());
         return view('pages.root');
     }
 }
