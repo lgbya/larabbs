047bae27967152256cf30a34552a4d7f3667fa58
lgbya <lgbya1105882406@aliyun.com>
2020-07-28 00:42:49 +0800

    话题列表页面

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = app(Generator::class);
+
+        $avatars = [
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png',
+        ];
+
+        $users = factory(User::class)
+            ->times(10)
+            ->make()
+            ->each(function ($user, $index) use ($faker, $avatars){
+                $user->avatar = $faker->randomElement($avatars);
+            });
+
+        $userArray = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // 插入到数据库中
+        User::insert($userArray);
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'Summer';
+        $user->email = 'summer@example.com';
+        $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+
+    }
+}
