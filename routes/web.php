75f9f086bfa73626ff24625b6b51084d6078c75a
lgbya <lgbya1105882406@aliyun.com>
2020-07-28 05:51:33 +0800

    翻译 URL 标示

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,8 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 Route::post('topics_upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
