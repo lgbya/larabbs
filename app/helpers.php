a6442df90aafe9a431bc47a5c89b166e06c2eb7a
lgbya <lgbya1105882406@aliyun.com>
2020-07-29 11:35:23 +0800

    内容管理后台

--- app/helpers.php
+++ app/helpers.php
@@ -18,3 +18,29 @@ function make_excerpt($value, $length = 200)
     $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
     return Str::limit($excerpt, $length);
 }
+
+function model_admin_link($title, $model)
+{
+    return model_link($title, $model, 'admin');
+}
+
+function model_link($title, $model, $prefix = '')
+{
+    $modelName = model_plural_name($model);
+
+    $prefix = $prefix ? "/$prefix/" : '/';
+
+    $url = config('app.url') . $prefix . $modelName . '/' . $model->id;
+
+    return '<a href="' . $url . '" target="_blank">' . $title . '</a>';
+}
+
+function model_plural_name($model)
+{
+    $fullClassName = get_class($model);
+
+    $className = class_basename($fullClassName);
+
+    $snakeCaseName = Str::snake($className);
+    return Str::plural($snakeCaseName);
+}
