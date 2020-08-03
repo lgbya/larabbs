e094007498655d14aeb5105c134b8910c9b2e97d
lgbya <lgbya1105882406@aliyun.com>
2020-08-04 00:40:57 +0800

    api/user

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array',
 ],function ($api){
     $api->group([
         'middleware' => 'api.throttle',
@@ -46,6 +47,12 @@ $api->version('v1', [
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
+
+
+        $api->group(['middleware' => 'api.auth'], function ($api){
+            $api->get('users', 'UsersController@me')
+                ->name('api.user.show');
+        });
 
     });
 
